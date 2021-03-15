7084b415c00ae49d6cc3ee22a8deeddf143c44e3
titoubravin <80425517+titoubravin@users.noreply.github.com>
2021-03-15 13:34:32 -0400

    Update amazon.py

--- src/scraper/amazon.py
+++ src/scraper/amazon.py
@@ -18,8 +18,8 @@ class AmazonScrapeResult(ScrapeResult):
         price_str = self.set_price(tag)
         if price_str:
             alert_subject = f'In Stock for {price_str}'
-        Else:
-            tag = self.soup.body.select_one('#aod-price-1 > span > span.a-offscreen')
+        else:
+        tag = self.soup.body.select_one('#aod-price-1 > span > span.a-offscreen')
         price_str = self.set_price(tag)
         if price_str:
             alert_subject = f'In Stock for {price_str}'
