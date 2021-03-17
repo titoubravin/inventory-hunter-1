e2bef8df539cc83dcef60a60d6dc0448295399c5
titoubravin <80425517+titoubravin@users.noreply.github.com>
2021-03-17 15:39:28 -0400

    Update amazon.py

--- src/scraper/amazon.py
+++ src/scraper/amazon.py
@@ -14,16 +14,10 @@ class AmazonScrapeResult(ScrapeResult):
             self.logger.warning(f'missing title: {self.url}')
 
         # get listed price
-        tag = self.soup.body.select_one('#aod-price-0 > span > span.a-offscreen')
+        tag = self.soup.body.find('#aod-price-0 > span > span.a-offscreen', '#aod-price-1 > span > span.a-offscreen')
         price_str = self.set_price(tag)
         if price_str:
             alert_subject = f'In Stock for {price_str}'
-            else:
-                if price_str > self.set_price(tag):
-                tag = self.soup.body.select_one('#aod-price-1 > span > span.a-offscreen')
-                price_str = self.set_price(tag)
-                if price_str:
-                alert_subject = f'In Stock for {price_str}'
             
         # check for add to cart button
         tag = self.soup.body.select_one('#aod-offer-price')
